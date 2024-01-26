d8be0d8aad17de31158157fe3c83502fa2621c02
Muhammadsardorprogrammer <137090102+Sardor-program@users.noreply.github.com>
2024-01-26 09:24:26 +0500

    first commit

---
+++ blog/models.py
@@ -0,0 +1,29 @@
+from PIL import Image
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    images = models.ImageField(default='post.jpg', upload_to='post_picture/', null=True, blank=True)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.images.path)
+
+        if img.height > 500 or img.width > 500:
+            output_size = (500, 500)
+            img.thumbnail(output_size)
+            img.save(self.images.path)
